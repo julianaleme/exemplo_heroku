7ced87cce22dd3761a7c87069903c19c7463879c
Juliana Leme <julianaleme@gmail.com>
2019-11-11 09:28:02 -0300

    "Commit"

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-var express = require('express');
-var app = express();
+const httpProxy = require("http-proxy");
+const puppeteer = require('puppeteer-extra')
 const port = process.env.PORT || 3000;
 
-app.get('/',function(req,res) {
-    res.send('Heroku teste');
-});
-
-app.listen(port, () => {
-    console.log('Servidor online porta ${port}!');
-});
+const host = "0.0.0.0";
+async function createServer(WSEndPoint, host, port) {
+  await httpProxy
+    .createServer({
+      target: WSEndPoint,
+      ws: true,
+      localAddress: host
+    })
+    .listen(port);
+  return `ws://${host}:${port}`;
+}
+puppeteer.launch({ 
+  headless: true,
+  defaultViewport:null,
+  devtools: false
+})
+.then(async browser => {
+    const pagesCount = (await browser.pages()).length; 
+    const browserWSEndpoint = await browser.wsEndpoint();
+    const customWSEndpoint = await createServer(browserWSEndpoint, host, port);
+    console.log({ browserWSEndpoint, customWSEndpoint, pagesCount });
+})
