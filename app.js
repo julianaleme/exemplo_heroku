bbbaacf70defb526d5445b2f9273aa3648579002
Juliana Leme <julianaleme@gmail.com>
2019-11-11 11:15:53 -0300

    Commit

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 var express = require('express');
-var app = express();
-const port = process.env.PORT || 3000;
+var path = require('path');
+var serveStatic = require('serve-static')
 
-app.get('/',function(req,res) {
-  res.send('HELLO')
-});
-
-app.listen(port, () => {
-  console.log(`Servidor online na porta ${port}!`);
-});
+app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started '+port);
