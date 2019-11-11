b6692a276a013700784c85fa457bc8188d4719e6
Juliana Leme <julianaleme@gmail.com>
2019-11-11 10:01:45 -0300

    Commit Atual

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
-const httpProxy = require("http-proxy");
-const puppeteer = require('puppeteer-extra')
+var express = require('express');
+var app = express();
 const port = process.env.PORT || 3000;
 
-const host = "0.0.0.0";
-async function createServer(WSEndPoint, host, port) {
-  await httpProxy
-    .createServer({
-      target: WSEndPoint,
-      ws: true,
-      localAddress: host
-    })
-    .listen(port);
-  return `ws://${host}:${port}`;
-}
-puppeteer.launch({ 
-  headless: true,
-  defaultViewport:null,
-  devtools: false
-})
-.then(async browser => {
-    const pagesCount = (await browser.pages()).length; 
-    const browserWSEndpoint = await browser.wsEndpoint();
-    const customWSEndpoint = await createServer(browserWSEndpoint, host, port);
-    console.log({ browserWSEndpoint, customWSEndpoint, pagesCount });
-})
+app.get('/',function(req,res) {
+  res.send('HELLO')
+});
+
+app.listen(port, () => {
+  console.log(`Servidor online na porta ${port}!`);
+});
